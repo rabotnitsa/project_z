938de1ce864a3f8978c6e32a05165f2c0519b05b
Amerikanka <taos.myers@gmail.com>
2021-04-01 00:58:58 -0500

    Update wireless.py

--- infoSec/wireless.py
+++ infoSec/wireless.py
@@ -11,11 +11,11 @@ import subprocess
 import optparse
 import re
 
+#defines user arguments
 def get_arguments():
-    # adding arguments for users
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Select wireless device")
-   # parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
     (options, arguments) = parser.parse_args()
     if not options.interface:
         parser.error("\n \n [-] Please specify an interface, use --help for more info.")
@@ -23,6 +23,7 @@ def get_arguments():
         parser.error("\n \n [-] Please specify a new mac address, use --help for more info.")
     return options
 
+#defining the interface that will be changed from "managed" to "monitor" mode
 def set_wireless(interface):
     print("\n \n [+] Puting " + interface + " into monitor mode ")
     subprocess.call(["ifconfig", interface, "down"])
